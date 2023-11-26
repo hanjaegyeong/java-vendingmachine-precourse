dea58a223bad1cf8f484817737be1261473968c1
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 13:34:42 +0900

    feat(domain): VendingMachine 보유금액 검증 기능 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -12,6 +12,8 @@ public class VendingMachine {
     private static EnumMap<Coin, Integer> vendingMachineCoins = new EnumMap<>(Coin.class);
 
     public void generateVendingMachineCoins(int vendingMachineAmount) {
+        validateVendingMachineCoins(vendingMachineAmount);
+
         List<Integer> coinTypes = Arrays.asList(500, 100, 50, 10);
         int remainingAmount = vendingMachineAmount;
 
@@ -22,6 +24,24 @@ public class VendingMachine {
                 vendingMachineCoins.put(coin, vendingMachineCoins.getOrDefault(coin, 0) + 1);
                 remainingAmount -= randomCoin;
             }
+        }
+    }
+
+    private void validateVendingMachineCoins(int vendingMachineAmount) {
+        validatePositiveNumber(vendingMachineAmount);
+        validateMultipleOfTen(vendingMachineAmount);
+    }
+
+    // 10의 배수이면 자동으로 양수 처리되는지 확인
+    private void validatePositiveNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("ERROR: 양수를 입력하여 주세요.");
+        }
+    }
+
+    private void validateMultipleOfTen(int number) {
+        if (number % 10 != 0) {
+            throw new IllegalArgumentException("ERROR: 10의 배수를 입력하여 주세요.");
         }
     }
 }

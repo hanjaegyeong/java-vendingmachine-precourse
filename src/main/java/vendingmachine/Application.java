37c0762f0fb85e9137bf9489678e92979eda114c
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 17:06:13 +0900

    feat(): Application 안쓰는 import 제거

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,16 +1,6 @@
 package vendingmachine;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import vendingmachine.controller.VendingMachineController;
-import vendingmachine.domain.Coin;
-import vendingmachine.domain.Product;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {

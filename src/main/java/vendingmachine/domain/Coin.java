0600d4e54e4f97dc71bae9f652dcb5d0c0ee8ff7
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 15:29:05 +0900

    feat(domain): Coin 클래스 findByAmount 기능 추가

---
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -0,0 +1,27 @@
+package vendingmachine.domain;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private final int amount;
+
+    Coin(final int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static Coin findByAmount(int amount) {
+        for (Coin coin : Coin.values()) {
+            if (coin.getAmount() == amount) {
+                return coin;
+            }
+        }
+        throw new IllegalArgumentException("에러메시지 작성");
+    }
+}

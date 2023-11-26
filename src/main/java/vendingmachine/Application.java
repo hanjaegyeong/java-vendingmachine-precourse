10ba2bc69e6a365e849c7abf94122b9805c5ec58
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 17:05:49 +0900

    feat(): Application 로직 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,20 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.controller.VendingMachineController;
+import vendingmachine.domain.Coin;
+import vendingmachine.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        VendingMachineController vendingMachineController = new VendingMachineController();
+        vendingMachineController.useVendingMachine();
     }
 }

fd8fab1d4f7f0f166498c96748cda39cced6f402
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 16:05:35 +0900

    feat(domain): PurchaseProcessor 구매 기능 추가
    - 구매 가능한지 검사 후 구매
    - 구매 시
      - 투입 금액 갱신
      - 해당상품 -1

--- src/main/java/vendingmachine/domain/PurchaseProcessor.java
+++ src/main/java/vendingmachine/domain/PurchaseProcessor.java
@@ -13,6 +13,19 @@ public class PurchaseProcessor {
     public String formatMachineInputAmountOutput() {
         return String.format("투입 금액: %d원", this.machineInputAmount);
     }
-    
 
+    // 구입 가능한지 검증부터 해야 함 - while(true) 안에서 돌도록
+    public boolean isPossiblePurchase(String productName, VendingMachineProduct vendingMachineProduct) {
+        if (machineInputAmount < vendingMachineProduct.getProductPriceByName(productName)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    // 구매 - 투입 금액 갱신 && 해당상품 -1
+    public void purchaseProduct(String productName, VendingMachineProduct vendingMachineProduct) {
+        machineInputAmount -= vendingMachineProduct.getProductPriceByName(productName);
+        vendingMachineProduct.decreaseQuantityOfProduct(productName);
+    }
 }

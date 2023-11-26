1fa459171b4b97715a4401ccc50309a13903f771
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-26 12:54:43 +0900

    feat(view): InputView 기능 추가

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,33 @@
+package vendingmachine.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    private static final String INPUT_AMOUNT_HELD_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_ITEM_HELD_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INPUT_PURCHASE_ITEM_MESSAGE = "구매할 상품명을 입력해 주세요.";
+    private static final String INVALID_AMOUNT_HELD_INPUT_MESSAGE = "[ERROR] 정수만을 입력해 주세요.";
+
+    public static int inputAmountHeld() {
+        System.out.println(INPUT_AMOUNT_HELD_MESSAGE);
+        return inputInt();
+    }
+
+    private static int inputInt() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_HELD_INPUT_MESSAGE);
+        }
+    }
+
+    public static String inputItemHeld() {
+        System.out.println(INPUT_ITEM_HELD_MESSAGE);
+        return readLine();
+    }
+
+    public static String inputPurchaseItem() {
+        System.out.println(INPUT_PURCHASE_ITEM_MESSAGE);
+        return readLine();
+    }
+}
